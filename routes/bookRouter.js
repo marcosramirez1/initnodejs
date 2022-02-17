a6e90aa23a649c3a43b90826f41891e8b211d32e
Marcos Ramirez <marcosramirez1@live.com.ar>
2022-02-17 11:36:27 -0300

    first commit

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const bookController = require('../controllers/bookController')
-const Joi = require('joi');
 const validator = require('express-joi-validation').createValidator();
-
-const bodySchema = Joi.object({
-	title: Joi.string().alphanum().required(),
-	author: Joi.string().min(3).max(30).required(),
-	genre: Joi.string().required(),
-	read: Joi.boolean().required()
-})
+const bookValidationer = require('../validations/bookValidationer')
 
 const routes = (Book) => {
 	const bookRouter = express.Router();
-
 	const { getBooks, postBooks, getBookById, putBooks, deleteBookById, getBookByAuthor, getBookByName } = bookController(Book); 
+	const {bodySchema, querySchema} = bookValidationer();
 
 	bookRouter.route('/books')
   .get(getBooks)
   .post(validator.body(bodySchema),  postBooks);
+
+	bookRouter.route('/books/search/author')
+	.get(validator.query(querySchema), getBookByAuthor)
+
+	bookRouter.route('/books/search/name')
+	.get(validator.query(querySchema), getBookByName)
 
 	bookRouter
 		.route('/books/:bookId')
@@ -25,11 +24,7 @@ const routes = (Book) => {
 		.put(putBooks)
 		.delete(deleteBookById)
 	
-	bookRouter.route('/books/:bookAuthor')
-	.get(getBookByAuthor)
 
-	bookRouter.route('/books/:bookAuthor')
-	.get(getBookByName)
 		
 		
 		return bookRouter;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,39 +1,27 @@
 const express = require('express');
 const userController = require('../controllers/userController')
-const Joi = require('joi');
 const validator = require('express-joi-validation').createValidator();
-
-const bodySchema = Joi.object({
-	firstName: Joi.string().alphanum().required(), 
-	lastName:  Joi.string().alphanum().required(),
-	userName: Joi.string().alphanum().required(),
-	password:  Joi.string().alphanum().required().min(8).max(20),
-	email: Joi.string().required()
-		.email({ minDomainSegments: 2, tlds: { allow: ['com', 'ar', 'net'] } }),
-	address: Joi.string(),
-	phone: Joi.number().required()
-})
-
-
+const userValidationer = require("../validations/userValidationer")
 
 const routes = (User) => {
 	const userRouter = express.Router();
 	const { getUsers, postUsers, getUserById, putUsers, deleteUserById, postLogin, getUserByUserName } = userController(User); 
+	const { querySchema, bodySchema } = userValidationer();
 
 	userRouter.route('/users')
   .get(getUsers)
 	.post(validator.body(bodySchema),  postUsers); 
 
+	userRouter.route('/users/search')
+		.get(validator.query(querySchema), getUserByUserName);
+
 	userRouter.route('/users/:userId')
 		.get(getUserById)
 		.put(putUsers)
-		.delete(deleteUserById)
-
-	userRouter.route('/users/:userName')
-		.get(getUserByUserName)
+		.delete(deleteUserById);
 
 	userRouter.route('/users/login')
-	.post(postLogin)
+	.post(postLogin);
 
 	
 		return userRouter;
